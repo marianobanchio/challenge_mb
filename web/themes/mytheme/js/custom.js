375939824c4f470f1c29d10df4343f96b6c806c2
Mariano Banchio <mbanchio@Marianos-MacBook-Air.local>
2023-03-04 20:42:50 -0300

    Dynamic sitename to js

--- web/themes/mytheme/js/custom.js
+++ web/themes/mytheme/js/custom.js
@@ -1,6 +1,10 @@
-let btn = document.createElement("button");
-btn.innerHTML = "Alert con el nombre del portal";
-btn.onclick = function () {
-    alert("Challenge MB Portal");
-  };
-document.body.appendChild(btn);
+(function ($, Drupal, drupalSettings) {
+
+    let btn = document.createElement("button");
+    btn.innerHTML = "Alert con el nombre del portal";
+    btn.onclick = function () {
+        alert(drupalSettings.challengemb.sitename);
+      };
+    document.body.appendChild(btn);
+
+})(jQuery, Drupal, drupalSettings);
