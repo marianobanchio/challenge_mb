8b6ad51597207c67ae265373478d0dd892ba8940
Mariano Banchio <mbanchio@Marianos-MacBook-Air.local>
2023-03-04 20:00:26 -0300

    Last changes

---
+++ web/themes/mytheme/js/custom.js
@@ -0,0 +1,6 @@
+let btn = document.createElement("button");
+btn.innerHTML = "Alert con el nombre del portal";
+btn.onclick = function () {
+    alert("Challenge MB Portal");
+  };
+document.body.appendChild(btn);
